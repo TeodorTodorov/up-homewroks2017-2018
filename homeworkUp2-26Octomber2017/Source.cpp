50cfaf3dc3dd55e5af096878ace30b18a8fe1cc8
TeodorTodorov <teodorvulchev@gmail.com>
2017-10-26 21:40:22 +0300

    up

--- homeworkUp2-26Octomber2017/Source.cpp
+++ homeworkUp2-26Octomber2017/Source.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 void printPascalRow(int n) {
-
+/* TODO: FIX THAT UGLY PEACE OF CODE*/
 	const char dev = ' ';
 	int last = 1;
 	cout << last << dev << dev;
